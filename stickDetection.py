ad0368f294e3e6d6b749e36ef1ea85c8a3387241
coodyz <43947544+coolbreeze2@users.noreply.github.com>
2019-08-03 20:35:09 +0800

    Update stickDetection.py

--- stickDetection.py
+++ stickDetection.py
@@ -1,138 +1,175 @@
 # coding=utf-8
+import math
+import time
+from naoqi import ALProxy
+import almath
+from PIL import Image
 import cv2
 import numpy as np
-import time
 
 
-class StickDetection(object):
-    def __init__(self, img):
+class ComputeHitBallPosition(object):
+    """
+    调用computeHitBallPosition2方法，获取球洞坐标，红球坐标， 击球点坐标
+    """
+
+    def __init__(self, robotIp, port, landmarkTheoreticalSize=0.105, HitBallD=0.10):
+        self.robotIp = robotIp
+        self.port = port
+        self.landmarkTheoreticalSize = landmarkTheoreticalSize
+        self.HitBallD = HitBallD
+        self.cameraAngleX = 60.97 * math.pi / 180
+        self.cameraAngleY = 47.64 * math.pi / 180
+
+    def __RedBallDetect(self, img, imageWidth, imageHeight):
         """
-        :param img: img path
+        :param img: cv2 Image
+        :param imageWidth: image Width
+        :param imageHeight: image Height
+        :return: (1,center) or (None,None), Red Ball center
         """
-        self.img = img
-        self.low_range = np.array([27, 55, 115])
-        self.high_range = np.array([40, 255, 255])
+        flag = 0
+        hue_image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        gaus = cv2.GaussianBlur(hue_image, (7, 7), 1.5)
+        # 用颜色分割图像
+        low_range = np.array([0, 123, 100])
+        high_range = np.array([5, 255, 255])
+        th = cv2.inRange(gaus, low_range, high_range)
 
-    def bilateralFilter(self, img):
-        bilateral = cv2.bilateralFilter(img, 25, 12.5, 50)
-        # cv2.imshow("src", img)
-        # cv2.imshow("bilateral", bilateral)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-        return bilateral
+        # 形态学运算，膨胀
+        dilated = cv2.dilate(th, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)), iterations=2)
 
-    def __brightEnhance(self, img, alpha=1.5, beta=1.0):
+        # Hough Circle
+        circles = cv2.HoughCircles(dilated, cv2.HOUGH_GRADIENT, 1, 100, param1=15, param2=7, minRadius=10,
+                                   maxRadius=100)
+
+        # 绘制
+        center = (0, 0)
+        if circles is not None:
+            x, y, radius = circles[0][0]
+            center = (x, y)
+            cv2.circle(img, center, radius, (0, 255, 0), 2)
+            flag = 1
+
+        cv2.line(img, (imageWidth / 2, 0), (imageWidth / 2, imageHeight), (255, 0, 0), 1)
+        cv2.line(img, (0, imageHeight / 2), (imageWidth, imageHeight / 2), (255, 0, 0), 1)
+        cv2.imshow('Result', img)
+        cv2.imshow('Binaryzation', th)
+        cv2.waitKey()
+
+        if flag:
+            return 1, center
+        if not flag:
+            return None, None
+
+    def __computeRedBall(self, ballR=0.018):
         """
-        亮度增强
-        :param img: img array
-        :param alpha: multi
-        :param beta: Bias
-        :return:
+        :return:Red Ball x, y, distance
         """
-        img1 = img.copy()
-        # img1 = img1 * alpha + beta
-        img1 = np.where(img1 <= 50, img1, img1 * alpha + beta)
-        img1 = np.where(img1 <= 255, img1, 255 * 2 - img1)
-        img1 = np.array(img1, dtype=np.uint8)
-        return img1
+        camProxy = ALProxy("ALVideoDevice", self.robotIp, 9559)
+        motionProxy = ALProxy("ALMotion", self.robotIp, 9559)
+        camProxy.setActiveCamera(1)
+        cameraId = 1
+        resolution = 2  # VGA 解析度
+        colorSpace = 11  # RGB
+        fps = 5
+        subscriberID = "subscriberID"
 
-    def __compute_score(self, hsv, x, y, w, h):
-        """
-        :param x: box minx
-        :param y: box miny
-        :param hsv: hsv array
-        :param w: box width
-        :param h: box height
-        :return: score
-        """
-        score = 0
-        for j in range(x, x + w):
-            for i in range(y, y + h):
-                if i < len(hsv[0]) and j < len(hsv):
-                    if self.low_range[0] < hsv[i][j][0] < self.high_range[0] and self.low_range[1] < hsv[i][j][1] < \
-                            self.high_range[1] and self.low_range[2] < hsv[i][j][2] < self.high_range[2]:
-                        score += 1
-        return h
-        # int(100 * (float(score) / (w * h)))
-        # score
-        # w * h
-        # h
+        subscriberID = camProxy.subscribeCamera(subscriberID, cameraId, resolution, colorSpace, fps)
+        naoImage = camProxy.getImageRemote(subscriberID)
 
-    def nms(self, score_list):
-        """
-        nms非极大值抑制
-        :param score_list: score list
-        :return: x, y, w, h
-        """
-        score_list.sort(
-            cmp=lambda score1, score2: score2[4] - score1[4])
-        # score2[4] - score1[4]
-        # int(100 * (abs(score1[2] / score1[3] - 0.1) - abs(score2[2] / score2[3] - 0.1)))
-        return score_list[0][0], score_list[0][1], score_list[0][2], score_list[0][3]
+        camProxy.releaseImage(subscriberID)
+        camProxy.unsubscribe(subscriberID)
+        imageWidth = naoImage[0]
+        imageHeight = naoImage[1]
+        image_bytes = naoImage[6]
 
-    def stick_detection(self):
-        """
-        stick detection
-        :return: stick center
-        """
-        self.img = cv2.imread(self.img, 1)
-        if len(self.img) != 640:
-            self.img = cv2.resize(self.img, (640, 480))
-        self.img = self.bilateralFilter(self.img)
-        # self.img = self.__brightEnhance(self.img)
-        hsv = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
+        img_np = Image.frombytes("RGB", (imageWidth, imageHeight), image_bytes)
+        cameraPosition = motionProxy.getPosition("CameraBottom", 2, True)
+        cameraX = cameraPosition[0]
+        cameraY = cameraPosition[1]
+        print("cameraX: ", cameraX)
+        cameraHeight = cameraPosition[2]
+        print("cameraHeight: ", cameraHeight)
 
-        kernelDilation = np.ones((5, 5), dtype=np.uint8)
+        # PIL image 转cv2 image
+        open_cv_image = np.array(img_np)
+        img1 = cv2.cvtColor(open_cv_image, cv2.COLOR_RGB2BGR)
+        RedBall, center = self.__RedBallDetect(img1, imageWidth, imageHeight)
 
-        th = cv2.inRange(hsv, self.low_range, self.high_range)
-
-        # dilated = cv2.dilate(th, kernelDilation, iterations=1)
-        # gaus = cv2.GaussianBlur(th, (9, 9), 0)
-
-        _, contours, _ = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        # draw_img = cv2.drawContours(hsv.copy(), contours, 2, (0, 255, 0), 1)
-        num = len(contours)
-        # print("num: ", num)
-        # nms非极大值抑制，score为hsv黄色像素
-        score_list = list()
-        for i in range(num):
-            x, y, w, h = cv2.boundingRect(contours[i])
-            score = self.__compute_score(hsv, x, y, w, h)
-            score_list.append([x, y, w, h, score])
-            # cv2.circle(self.img, (x + w / 2, y + h / 2), 4, (255, 0, 0), -1)
-            # cv2.rectangle(self.img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-        if len(score_list) > 1:
-            x, y, w, h = self.nms(score_list)
-            center = (x + w / 2, y + h / 2)
-            cv2.circle(self.img, center, 4, (255, 0, 0), -1)
-            cv2.rectangle(self.img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            # cv2.namedWindow("result", False)
-            # cv2.imshow("result", self.img)
-            # cv2.moveWindow("result", 0, 0)
-            # cv2.waitKey()
-            # cv2.imwrite("img/stick1_ed.jpg", self.img)
-            # cv2.destroyAllWindows()
-            return center
+        if not RedBall:
+            print("none")
+            return None, None, None
         else:
-            return 0, 0
+            print("center: {}".format(center))
+            ballAngleX = cameraX + (imageWidth / 2 - center[0]) / imageWidth * self.cameraAngleX
+            print("ballAngleX: ", ballAngleX)
+            ballAngleY = cameraY + (center[1] - imageHeight / 2) / imageHeight * self.cameraAngleY
+            print("ballAngleY: ", ballAngleY)
+            ballDistance = math.tan(ballAngleY) * (cameraHeight - ballR)
+            print("ballDistance: ", ballDistance)
+            ballX = ballDistance * math.sin(ballAngleX)
+            ballY = ballDistance * math.cos(ballAngleY)
+            return ballX, ballY, ballDistance
 
-    def save_img(self, path):
-        cv2.imwrite(path, self.img)
+    def __computeMarkDistance(self):
+        """
+        :return: HoleX, HoleY, MarkDistance
+        """
+        currentCamera = "CameraTop"
+        memoryProxy = ALProxy("ALMemory", self.robotIp, self.port)
+        landmarkProxy = ALProxy("ALLandMarkDetection", self.robotIp, self.port)
+        landmark = landmarkProxy.subscribe("landmarkTest")
+        if landmark is not None:
+            print("subscribe Fail")
+        markData = memoryProxy.getData("LandmarkDetected")
+        while markData is None or len(markData) == 0:
+            time.sleep(0.01)
+            markData = memoryProxy.getData("LandmarkDetected")
+        landmarkProxy.unsubscribe("landmarkTest")
+        wzCamera = markData[1][0][0][1]
+        wyCamera = markData[1][0][0][2]
+        angularSize = markData[1][0][0][3]
+        distanceFromCameraToLandmark = self.landmarkTheoreticalSize / (2 * math.tan(angularSize / 2))
+        motionProxy = ALProxy("ALMotion", self.robotIp, self.port)
+        transform = motionProxy.getTransform(currentCamera, 2, True)
+        transformList = almath.vectorFloat(transform)
+        robotToCamera = almath.Transform(transformList)
+        cameraToLandmarkRotationTransform = almath.Transform_from3DRotation(0, wyCamera, wzCamera)
+        cameraToLandmarkTranslationTransform = almath.Transform(distanceFromCameraToLandmark, 0, 0)
+        robotToLandmark = robotToCamera * cameraToLandmarkRotationTransform * cameraToLandmarkTranslationTransform
+        x = robotToLandmark.r1_c4
+        y = robotToLandmark.r2_c4
+        print "x " + str(robotToLandmark.r1_c4) + " (in meters)"
+        print "y " + str(robotToLandmark.r2_c4) + " (in meters)"
+        print "z " + str(robotToLandmark.r3_c4) + " (in meters)"
+        distance = math.sqrt(robotToLandmark.r1_c4 ** 2 + robotToLandmark.r2_c4 ** 2)
+        return x, y, distance
+
+    def computeHitBallPosition2(self):
+        """
+        调用此方法，获取球洞坐标，红球坐标， 击球点坐标
+        :return: A dict, HoleX, HoleY, ballX, ballY, HitX, HitY
+        """
+        hb = self.HitBallD  # The distance from the hitting point to the ball
+        HoleX, HoleY, MarkD = self.__computeMarkDistance()
+
+        BallX, BallY, BallD = self.__computeRedBall()
+        k = (HoleY - BallY) / (HoleX - BallX)
+
+        HitX = math.sqrt(abs(math.sqrt(hb) / (k + 1))) + BallX  # Hit ball positions
+        HitY = k * (math.sqrt(abs(math.sqrt(hb) / (k + 1)))) + BallY
+
+        return {"HoleX": HoleX, "HoleY": HoleY, "BallX": BallX, "BallY": BallY, "HitX": HitX, "HitY": HitY}
 
 
 if __name__ == '__main__':
-    stime = time.time()
-    for z in range(1, 341):
-        imgPath = "stickImg/stick_{}.jpg".format(z)
-        stick = StickDetection(imgPath)
-
-        stick_center = stick.stick_detection()
-        stick.save_img("detectedImg/{}".format(imgPath.split('/')[-1]))
-
-        if stick_center != (0, 0):
-            print("times: {}  center: {}".format(z, stick_center))
-        else:
-            print "Not Found stick"
-    etime = time.time()
-    print("Time spent: {}s".format(etime - stime))
+    HitBall = ComputeHitBallPosition("192.168.1.104", 9559)
+    HitBallDict = HitBall.computeHitBallPosition2()
+    Holex = HitBallDict["HoleX"]
+    Holey = HitBallDict["HoleY"]
+    ballx = HitBallDict["BallX"]
+    bally = HitBallDict["BallY"]
+    Hitx = HitBallDict["HitX"]
+    Hity = HitBallDict["HitY"]
+    print("\nHoleX:{}\nHoleY:{}\nballX:{}\nballY:{}\nHitX:{}\nHitY:{}".format(Holex, Holey, ballx, bally, Hitx, Hity))
